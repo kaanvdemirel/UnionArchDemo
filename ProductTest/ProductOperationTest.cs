e353f733a6085cda0019c966afc4710800f9bdf2
kaan.demirel <kaan.demirel@TEST09>
2024-09-03 12:58:17 +0300

    Unit Test Added.

---
+++ ProductTest/ProductOperationTest.cs
@@ -0,0 +1,50 @@
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+
+namespace ProductTest
+{ 
+    [TestFixture]
+    public class ProductOperationTest
+    {
+        private ProductManager _productManager;
+        private EfProductDal _efProductDal;
+
+
+
+        [SetUp]
+
+
+        public void Setup()
+        {
+
+            _efProductDal = new EfProductDal();
+            _productManager = new ProductManager( _efProductDal );  
+
+
+
+        }
+
+
+
+
+
+
+
+
+        [Test]
+        public void Product_ListedAllProducts_GetAll()
+        {
+            IProductService productService = new ProductManager(new EfProductDal());
+
+            var products = _productManager.GetAll();
+
+
+            Assert.IsNotNull( products,"Ürünler null olmamalýdýr." );
+
+           
+
+
+        }
+    }
+}
